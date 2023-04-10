3045ae99891c2c05373ecb7097bbb40d5dd2d348
Maksim Aptishev GitHub <maximus.testqa777@gmail.com>
2023-04-11 01:08:35 +0300

    initial commit

---
+++ src/test/java/ru/netology/javaqa/BonusServiceTest.java
@@ -0,0 +1,103 @@
+package ru.netology.javaqa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BonusServiceTest {
+
+    @Test
+    public void shouldCalculateForRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 30;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public  void shouldCalculateForRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1_000_000;
+        boolean registered = true;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testBoundaryValues1() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 100;
+        boolean registered = true;
+        long expected = 3;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testBoundaryValues2() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 16000;
+        boolean registered = true;
+        long expected = 480;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testBoundaryValues3() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 49900;
+        boolean registered = false;
+        long expected = 499;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testBoundaryValues4() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 100;
+        boolean registered = false;
+        long expected = 1;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+}
